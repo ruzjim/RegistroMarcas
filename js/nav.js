8da95850d209253e67a66e57a02ca4e25433ab1d
ruzjim <162730248+ruzjim@users.noreply.github.com>
2025-03-08 13:34:22 -0600

    mejora estructura

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,20 @@
-
 // Cargar el nav en el contenedor de la pagina que lo llamen
-    fetch('components/nav.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('nav-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error al cargar el nav:', error));
+fetch("components/nav.html")
+  .then((response) => response.text())
+  .then((data) => {
+    console.log(data);
+    
+    document.getElementById("nav-container").innerHTML = data;
+  })
+  .catch((error) => console.error("Error al cargar el nav:", error));
 
-
-        prueba();
-        function prueba(){
-            console.log('prueba');
-            
-        }
+// $.ajax({
+//   url: "components/nav.html",
+//   method: "GET",
+//   success: function (data) {
+//     $("#nav-container").html(data);
+//   },
+//   error: function (error) {
+//     console.error("Error al cargar el nav:", error);
+//   },
+// });
